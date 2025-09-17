7aed6b65242ff4ee265b7da1fae93389e337438a
Nihar688 <Nihar20.kaushik@gmail.com>
2025-09-17 10:51:27 -0400

    factors of a number

---
+++ p4.py
@@ -0,0 +1,18 @@
+#Factors of a number
+import math
+start = 1
+num = int(input("enter a value: "))
+new_stop = int(math.sqrt(num)) + 1
+factor_count = 0
+while start < new_stop:
+    if num % start == 0:
+        dividend = num // start
+        if start != dividend:
+            factor_count += 2
+        else:
+            factor_count += 2
+    start += 1
+print(f"{num} has {factor_count} many factors.")
+
+
+    
