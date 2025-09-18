cd56f4a2635031543482d14ac4dbad4a87d118d0
Nihar688 <Nihar20.kaushik@gmail.com>
2025-09-18 10:58:54 -0400

    New changes

---
+++ snake.py
@@ -0,0 +1,26 @@
+#Snakes & Ladders 
+
+while True: 
+    Current_position = 1 
+    roll = int(input("Enter dice roll data: "))
+
+if square + roll == 100:
+    print (f"You are now on square {square+roll}")
+    print ("You Win!")
+else:
+    new_position == square + roll
+    if new_position == 9: 
+        square = 34
+    elif new_position == 54:
+        square = 19
+    elif new_position == 64:
+        square = 40
+    elif new_position == 86:
+        square = 67
+    elif new_position == 90:
+        square = 48
+    elif new_position == 77:
+        square = 99 
+
+
+        
