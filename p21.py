3f5d4241a8e6183420c9c2267b89519a6268e1d1
Nihar688 <Nihar20.kaushik@gmail.com>
2025-10-27 10:06:10 -0400

    bubble code

---
+++ p21.py
@@ -0,0 +1,8 @@
+def bubble(a_list): 
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(1, len(a_list)):
+            if a_list[i-1] > a_list[i]:
+                swapped = True
+                a_list[i-1], a_list[i] = a_list[i], a_list[i-1]
